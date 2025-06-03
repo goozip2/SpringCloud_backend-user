ce6f703f875168ae9b5962de8b7d315e2583a43c
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-06-03 14:26:39 +0900

    feat: request header에서 사용자 정보를 추출하는 util 클래스 구현 및 테스트용 controller method 작성

--- src/main/java/com/welab/backend_user/api/open/UserController.java
+++ src/main/java/com/welab/backend_user/api/open/UserController.java
@@ -1,6 +1,7 @@
 package com.welab.backend_user.api.open;
 
 import com.welab.backend_user.common.dto.ApiResponseDto;
+import com.welab.backend_user.common.web.context.GatewayRequestHeaderUtils;
 import com.welab.backend_user.remote.alim.RemoteAlimService;
 import com.welab.backend_user.remote.alim.dto.AlimSendSmsDto;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +21,19 @@ public class UserController {
         String response = remoteAlimService.hello();
         return ApiResponseDto.createOk(response);
     }
+    @GetMapping(value = "/testAuth")
+    public ApiResponseDto<String> testAuth() {
+        // String response = remoteAlimService.sms();
+        String userId = GatewayRequestHeaderUtils.getUserIdOrThrowException();
+        log.info("userId = {}", userId);
+        return ApiResponseDto.createOk(userId);
+    }
 
     @PostMapping(value = "/sms")
     public ApiResponseDto<AlimSendSmsDto.Response> sms(@RequestBody AlimSendSmsDto.Request request) {
         var response = remoteAlimService.sendSms(request);
         return ApiResponseDto.createOk(response);
     }
+
+
 }
